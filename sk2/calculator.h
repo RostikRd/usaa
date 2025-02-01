e7169f668c14d0458afd0552ebc698bbf6159dc5
RostikRd <140062312+RostikRd@users.noreply.github.com>
2025-02-01 14:28:02 +0200

    Create calculator.h

---
+++ sk2/calculator.h
@@ -0,0 +1,9 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#define length_of_exercise 100
+
+double main_calculation(const char* expr);
+int check_zatvorky(const char* input);
+
+#endif // CALCULATOR_H
